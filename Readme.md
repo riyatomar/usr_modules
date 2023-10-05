4fa3f06751e7a80e3f0c0c7f4ac8096a5913cae0
Riya <riyatomar912@gmail.com>
2023-10-05 11:34:09 +0530

    USRs modules

---
+++ Readme.md
@@ -0,0 +1,5 @@
+1. Command to generate concept row:
+    python concepts.py 
+
+2. Command to generate rows after concept row:
+    python row3_to_row11.py
---
+++ row3_to_row11.py
@@ -0,0 +1,22 @@
+import os
+
+scripts=[
+    "indexing.py",
+    "sem_cat.py",
+    "morpho_sem.py",
+    "dependency.py",
+    "discourse.py",
+    "spk_view.py",
+    "scope.py",
+    "sent_type.py"
+]
+
+def run_script(script_name):
+    try:
+        command=f"python3 {script_name}"
+        os.system(command)
+    except Exception as e:
+        print(f"Error running script: {e}")
+
+for script in scripts:
+    run_script(script)
